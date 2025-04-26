2b8fe8fcbcd29a0e2bad90819dce1af970eaf9cb
Sr33Rag <sreeragpmohan15@gmail.com>
2025-04-26 22:39:34 +0530

    New Build

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRoutes from './AppRoutes';
 import { AppProvider } from './context/AppContext';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
